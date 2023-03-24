b23241d696d65f1bc4be280867651e76ed1bb061
Sojung Lee <90453158+xxoznge@users.noreply.github.com>
2023-03-25 01:09:19 +0900

    Entity student

--- src/main/java/com/example/springboot_class/Entity/student.java
+++ src/main/java/com/example/springboot_class/Entity/student.java
@@ -2,13 +2,12 @@ package com.example.springboot_class.Entity;
 
 import javax.persistence.*;
 
-@Table
+@Table(name="student")
 @Entity
 public class student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column
     private String name;
 
